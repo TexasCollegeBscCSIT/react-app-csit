83d041d887e7ced5d0177cf0e59f095cb2ab38bc
Subodh2084 <subodhnepal10@gmail.com>
2024-07-22 07:22:36 +0545

    merging

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -9,11 +9,8 @@ export default function Logo() {
           className="uppercase animate-text bg-gradient-to-r from-black/60 via-white/10 to-black bg-clip-text text-transparent text-5xl font-black text-center"
         >
           &nbsp;S-Register<span className="text-5xl"></span>&nbsp;
-    <div className="max-w-64 absolute">
-      <div id="logo" className='grid grid-flow-row content-start'>
-        <h2 data-text="T&#8709;d&#8709;" className="uppercase animate-text bg-gradient-to-r from-black/60 via-white/10 to-black bg-clip-text text-transparent text-5xl font-black text-center">
-          &nbsp;T&#8709;d&#8709;<span className='text-5xl'></span>&nbsp;
         </h2>
+
       </div>
     </div>
   );
