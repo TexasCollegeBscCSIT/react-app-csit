c48f8e6d950d35390d946003ba928fa4ddea24d7
Hari Upreti <hariupreti679@gmail.com>
2024-07-23 08:18:12 +0545

    Updated after implementing dark mood

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
 import Logo from "../logo/Logo";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
 
-export default function Header() {
+export default function Header({ scrollValue }) {
+  console.log(scrollValue);
+  const [enabled, setEnabled] = useState(false);
   const listStyle =
     // "list-none float-left text-md uppercase cursor-pointer text-black hover:text-slate-300 ml-28 transition-all";
-    "list-none float-left text-lg uppercase font-bold cursor-pointer  ml-28 ";
+    "list-none float-left text-lg uppercase font-bold cursor-pointer  ml-28 dark:text-white";
 
+  const toggleLightDarkMode = () => {
+    setEnabled(!enabled);
+    console.log(enabled);
+    const newMood = enabled ? "light" : "dark";
+    localStorage.setItem("theme-mood", newMood);
+    document.getElementById("main-body")?.setAttribute("data-mode", newMood);
+  };
   return (
-    <div className="bg-white">
+    <div
+      className={`bg-white dark:bg-black  w-full transition-all duration-700 ${
+        scrollValue >= 20 ? "fixed" : "relative"
+      }`}
+    >
       <div className=" w-full h-24 grid grid-flow-col container mx-auto overflow-hidden">
         <div className="col-span-2">
           <Logo></Logo>
@@ -29,13 +44,24 @@ export default function Header() {
           </ul>
         </div>
         <div className="col-span-3">
-          <ul className="my-9 text-center  border-gray-700">
-            <Link
-              to={"/register"}
-              className="mx-6 text-black px-3 py-2 rounded-2xl bg-cyan-600/90 hover:bg-cyan-500 font-bold transition-all cursor-pointer"
-            >
-              Register
-            </Link>
+          <ul className="my-9 mx-8 grid grid-flow-col border-gray-700">
+            <li>
+              <Link
+                to={"/register"}
+                className="mx-6 text-black px-3 py-2 rounded-2xl bg-cyan-600/90 hover:bg-cyan-500 font-bold transition-all cursor-pointer"
+              >
+                Register
+              </Link>
+            </li>
+            <li>
+              <Switch
+                checked={enabled}
+                onChange={toggleLightDarkMode}
+                className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600"
+              >
+                <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
+              </Switch>
+            </li>
           </ul>
         </div>
       </div>
--- src/layouts/GuestLayout.tsx
+++ src/layouts/GuestLayout.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Header from "../components/navbar/Header";
 
 export default function GuestLayout({ children }) {
+  const [scrollValue, setScrollValue] = useState(0);
+  const handleScroll = (e) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target;
+    const position = Math.ceil(
+      (scrollTop / (scrollHeight - clientHeight)) * 100
+    );
+    setScrollValue(position);
+  };
+
   return (
-    <div className="max-h-screen max-w-screen overflow-y-auto overflow-x-hidden">
-      <Header></Header>
+    <div
+      onScroll={(e) => handleScroll(e)}
+      className="max-h-screen max-w-screen overflow-y-auto overflow-x-hidden"
+    >
+      <Header scrollValue={scrollValue}></Header>
       <div className="bg-black/75 h-screen w-screen">
-        <div className=" mx-auto">{children}</div>
+        <div
+          className={`mx-auto transition-all duration-700 ${
+            scrollValue >= 18 ? "mt-12" : "mt-0"
+          }`}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
